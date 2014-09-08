d98ee7711e9e826c9b238bf0fa06ed0a961a12c9
Saravanakumar <saravana@RSaravanakumar.local>
2014-09-08 19:49:01 +0530

    TEST

---
+++ sample.rb
@@ -0,0 +1,4 @@
+      action_hash = automation_actions watcher_agents
+      action_hash = action_hash.select{ |action| action.fetch(:condition, true) }
+      add_custom_actions action_hash
+      @action_defs = ActiveSupport::JSON.encode action_hash  
